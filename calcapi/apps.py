a0934e31cebcb600044fb414f959194103370a4c
Luke Hardy <luke.taylor.hardy@gmail.com>
2024-05-14 14:27:27 -0400

    added totalbf calculation to inventory GET request views

--- calcapi/apps.py
+++ calcapi/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.core.management import call_command
 
 
 class CalcapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'calcapi'
+
+    def ready(self):
+        post_migrate.connect(run_recalculate_totalbf, sender=self)
+
+def run_recalculate_totalbf(sender, **kwargs):
+    call_command('recalculate_totalbf')
